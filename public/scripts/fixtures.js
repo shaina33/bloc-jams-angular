b1570aace140e8c76d4ee5e655721cfa84b1dc75
Shaina Karasin <skarasin39@gmail.com>
2016-07-28 14:44:58 -0400

    checkpoint #5

---
+++ public/scripts/fixtures.js
@@ -0,0 +1,42 @@
+// Example Data (in place of database)
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioURL: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioURL: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioURL: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioURL: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioURL: 'assets/music/magenta' }
+     ]
+};
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+var albumATE = {
+    title: 'Songs of God and Whiskey',
+    artist: 'The Airborne Toxic Event',
+    label: 'Epic',
+    year: '2015',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Song 1', duration: '4:26' },
+        { title: 'Song 2', duration: '3:14' },
+        { title: 'Song 3', duration: '5:01' },
+        { title: 'Song 4', duration: '3:21'}
+     ]
+};
